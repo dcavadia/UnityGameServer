4e33ab132412c0f469a3d62a99d61d920533410c
dcavadia <dan.acppp@gmail.com>
2023-01-09 02:31:07 -0400

    add auth request + response

---
+++ SharedLibrary/Responses/AuthenticationResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLibrary.Responses
+{
+    public class AuthenticationResponse
+    {
+        public string Token { get; set; }
+    }
+}
